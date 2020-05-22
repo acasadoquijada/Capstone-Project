25784f5b7709b561fdeb809f0b663da0787f63d7
acasadoquijada <acasadoquijada@gmail.com>
2020-05-22 10:49:50 +0200

    feat: add missing elements PodcastEpisode
    The PodcastEpisode model was missing some elements (name, description,
    imageURL, mediaURL). This information is needed to be displayed in the
    screen.
    
    This commit adds this new members to the model solving the lack of
    information.
    
    Relate: #2

--- Podcasfy/app/src/main/java/com/example/podcasfy/model/PodcastEpisode.java
+++ Podcasfy/app/src/main/java/com/example/podcasfy/model/PodcastEpisode.java
@@ -5,11 +5,55 @@ public class PodcastEpisode {
     // This is the path where the mp3 file is stored in the device
     private String path;
 
+    // Podcast Episode name
+    private String name;
+
+    // Podcast Episode description
+    private String description;
+
+    // Podcast Episode image
+    private String imageURL;
+
+    // Podcast Episode media (audio)
+    private String mediaURL;
+
     public void setPath(String path) {
         this.path = path;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMediaURL(String mediaURL) {
+        this.mediaURL = mediaURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
     public String getPath() {
         return path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getMediaURL() {
+        return mediaURL;
+    }
+
+    public String getImageURL() {
+        return imageURL;
     }
 }
--- Podcasfy/app/src/test/java/com/example/podcasfy/PodcastEpisodeModelTest.java
+++ Podcasfy/app/src/test/java/com/example/podcasfy/PodcastEpisodeModelTest.java
@@ -10,14 +10,26 @@ import static org.junit.Assert.assertEquals;
 public class PodcastEpisodeModelTest {
 
     private String url = "folder/episode.mp3";
+    private String name = "planos and tests";
+    private String description = "description";
+    private String imageURL = "image.jpg";
+    private String mediaURL = "mediaURL";
 
     @Test public void PodcastEpisodeModelWorkAsExpected(){
 
         PodcastEpisode podcastEpisode = new PodcastEpisode();
 
         podcastEpisode.setPath(url);
+        podcastEpisode.setName(name);
+        podcastEpisode.setDescription(description);
+        podcastEpisode.setImageURL(imageURL);
+        podcastEpisode.setMediaURL(mediaURL);
 
         assertEquals(url,podcastEpisode.getPath());
+        assertEquals(name,podcastEpisode.getName());
+        assertEquals(description,podcastEpisode.getDescription());
+        assertEquals(mediaURL,podcastEpisode.getMediaURL());
+        assertEquals(imageURL,podcastEpisode.getImageURL());
 
     }
 }
