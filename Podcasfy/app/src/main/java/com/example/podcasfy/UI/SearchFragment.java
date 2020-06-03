ec0b3c733bc3762a51954505c950ffd7d1ae6368
acasadoquijada <acasadoquijada@gmail.com>
2020-06-03 16:19:47 +0200

    feat: add Search and Subscribed fragments
    This commit adds the files necessaries for the Search and subscribed
    fragments. It also modifies the nav_graph to allow the user navigate
    between the different fragments.
    
    This is an initial commit, there is more work to do for these fragments.
    
    Relates: #14 #15

---
+++ Podcasfy/app/src/main/java/com/example/podcasfy/UI/SearchFragment.java
@@ -0,0 +1,25 @@
+package com.example.podcasfy.UI;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.podcasfy.R;
+
+public class SearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.search, container, false);
+
+        return rootView;
+    }
+
+}
---
+++ Podcasfy/app/src/main/java/com/example/podcasfy/UI/SubscribedPodcastFragment.java
@@ -0,0 +1,25 @@
+package com.example.podcasfy.UI;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.podcasfy.R;
+
+public class SubscribedPodcastFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.subscribed_podcasts,container, false);
+
+        return rootView;
+    }
+}
