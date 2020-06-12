f9ed48a7d8e5c0dfad8662e311d30e0dcd3cbb24
acasadoquijada <acasadoquijada@gmail.com>
2020-06-12 18:01:40 +0200

    refactor: change organization PodcastFramgent methods
    The methods of this fragment have changed to match what is described in
    Uncle Bob's Clean Code book, each method should do only one thing.
    
    Relates: #12

--- Podcasfy/app/src/main/java/com/example/podcasfy/ui/PodcastFragment.java
+++ Podcasfy/app/src/main/java/com/example/podcasfy/ui/PodcastFragment.java
@@ -8,16 +8,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -25,11 +21,12 @@ import com.example.podcasfy.R;
 import com.example.podcasfy.adapter.PodcastEpisodeListAdapter;
 import com.example.podcasfy.databinding.PodcastFragmentBinding;
 import com.example.podcasfy.model.Podcast;
+import com.example.podcasfy.model.PodcastEpisode;
 import com.example.podcasfy.viewmodel.PodcastViewModel;
 import com.example.podcasfy.viewmodel.ReproducerViewModel;
 import com.squareup.picasso.Picasso;
 
-import java.util.Objects;
+import java.util.List;
 
 
 public class PodcastFragment extends Fragment implements PodcastEpisodeListAdapter.ItemClickListener {
@@ -37,11 +34,7 @@ public class PodcastFragment extends Fragment implements PodcastEpisodeListAdapt
     private PodcastViewModel mViewModel;
     private ReproducerViewModel reproducerViewModel;
     private PodcastFragmentBinding mBinding;
-    private String podcastID;
-    private PodcastEpisodeListAdapter podcastEpisodeListAdapter;
-
-    private EpisodeFragment.ItemClickListener mCallback;
-
+    private PodcastEpisodeListAdapter adapter;
 
     public interface ItemClickListener {
         void onItemClick(int clickedItem);
@@ -49,9 +42,6 @@ public class PodcastFragment extends Fragment implements PodcastEpisodeListAdapt
 
     public PodcastFragment(){
 
-    }
-    public static PodcastFragment newInstance() {
-        return new PodcastFragment();
     }
 
     @Override
@@ -62,82 +52,157 @@ public class PodcastFragment extends Fragment implements PodcastEpisodeListAdapt
         mBinding = DataBindingUtil.inflate(
                 inflater, R.layout.podcast_fragment, container, false);
 
-     //   mBinding = DataBindingUtil.setContentView(getActivity(),R.layout.podcast_fragment);
-
-        setHasOptionsMenu(true);
-
-      // setHasOptionsMenu(true);
-
-       // TextView v = rootView.findViewById(R.id.podcastNamePodcastFragment);
-
-        RecyclerView r1 = mBinding.getRoot().findViewById(R.id.episodeRecyclerView);
-
-        GridLayoutManager g = new GridLayoutManager(getContext(),1);
-
-        g.setOrientation(RecyclerView.VERTICAL);
-        podcastEpisodeListAdapter = new PodcastEpisodeListAdapter(this,false);
-
-        r1.setLayoutManager(g);
-
-        r1.setAdapter(podcastEpisodeListAdapter);
+        setupRecyclerView();
 
         return mBinding.getRoot();
     }
+
+    /**
+     * To setup the RecyclerViewEpisodes we create the necessary LayoutManager and PodcastListAdapter
+     */
+
+    private void setupRecyclerView(){
+
+        RecyclerView recyclerViewEpisodes = mBinding.getRoot().findViewById(R.id.episodeRecyclerView);
+
+        recyclerViewEpisodes.setLayoutManager(createGridLayoutManager());
+
+        recyclerViewEpisodes.setAdapter(createPodcastEpisodeListAdapter());
+    }
+
+    /**
+     * To create a GridLayoutManager with spanCount = 1 and horizontal orientation
+     * @return a GridLayoutManager object with spanCount = 1 and horizontal orientation
+     */
+    private GridLayoutManager createGridLayoutManager(){
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(),1);
+        gridLayoutManager.setOrientation(RecyclerView.VERTICAL);
+
+        return gridLayoutManager;
+
+    }
+    /**
+     * To create a PodcastEpisodeListAdapterw with OnClickListener but without swipeListener
+     * @return a PodcastEpisodeListAdapter without swipeListener
+     */
+    private PodcastEpisodeListAdapter createPodcastEpisodeListAdapter(){
+        adapter = new PodcastEpisodeListAdapter(this,false);
+        return adapter;
+    }
+
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.app_bar_menu, menu);
         super.onCreateOptionsMenu(menu,inflater);
-    }
+}
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         if(item.getItemId() == R.id.share_podcast){
-            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-            sharingIntent.setType("text/plain");
-            String shareBody = "Your body here";
-            String shareSub = "Your subject here";
-            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
-            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-            startActivity(Intent.createChooser(sharingIntent, "Share using"));
+            setupShareInformation();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * To create the information to be shared and create a implicit intent
+     */
+
+    private void setupShareInformation(){
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "Your body here";
+        String shareSub = "Your subject here";
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share using"));
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        createReproducerViewModel();
+        setupPodcastViewModel();
+    }
 
-        reproducerViewModel = new ViewModelProvider(requireActivity()).get(ReproducerViewModel.class);
+    /**
+     * To setup the PodcastViewModel, we create the PodcastViewModel and observe:
+     * - Podcast (podcast information)
+     * - Episodes (podcast episodes information)
+     */
+    private void setupPodcastViewModel(){
+        createPodcastViewModel();
+        observePodcast();
+        observeEpisodes();
+    }
 
-
+    private void createPodcastViewModel(){
         mViewModel = new ViewModelProvider(requireActivity()).get(PodcastViewModel.class);
-        mViewModel.setPodcastId(podcastID);
+    }
 
-        mViewModel.getPodcast().observe(getViewLifecycleOwner(), podcast -> {
-            getActivity().setTitle(podcast.getName());
-            mBinding.podcastDescription.setText(podcast.getDescription());
-            Picasso.get().load(podcast.getMediaURL()).into(mBinding.podcastLogo);
-        });
+    /**
+     * To observe the podcast information and update the UI
+     */
+    private void observePodcast(){
+        mViewModel.getPodcast().observe(getViewLifecycleOwner(), this::updateUI);
+    }
 
-        mViewModel.getPodcastEpisode().observe(getViewLifecycleOwner(), podcastEpisodes -> {
-            // Here I update the podcastepisodes in
-            podcastEpisodeListAdapter.setPodcastEpisodes(podcastEpisodes);
-        });
+    /**
+     * Updates the UI with the podcast information
+     */
 
+    private void updateUI(Podcast podcast){
+        setActivityTitle(podcast.getName());
+        setDescription(podcast.getDescription());
+        setLogo(podcast.getMediaURL());
+    }
+
+    private void setActivityTitle(String name){
+        requireActivity().setTitle(name);
+    }
+
+    private void setDescription(String description){
+        mBinding.podcastDescription.setText(description);
+    }
+
+    private void setLogo(String media){
+        Picasso.get().load(media).into(mBinding.podcastLogo);
+    }
+
+    /**
+     * To observe the podcast episodes and updates the adapter
+     */
+    private void observeEpisodes(){
+        mViewModel.getPodcastEpisode().observe(getViewLifecycleOwner(), this::updateAdapterEpisodes);
+    }
+
+    private void updateAdapterEpisodes(List<PodcastEpisode> podcastEpisodesList){
+        adapter.setPodcastEpisodes(podcastEpisodesList);
+    }
+
+    /**
+     * To create the ReproducerViewModel
+     */
+    private void createReproducerViewModel(){
+        reproducerViewModel = new ViewModelProvider(requireActivity()).get(ReproducerViewModel.class);
     }
 
     @Override
     public void onItemClick(int clickedItem) {
+        updateName("pepe");
 
-        reproducerViewModel.setName("pepe");
+    }
 
-
-   /*     NavDirections action =
-                PodcastFragmentDirections.actionPodcastFragmentToPodcastEpsiodeFragment("id");
-        NavHostFragment.findNavController(this).navigate(action);*/
+    /**
+     * To update the ViewModel name field with the name of the selected episode
+     * @param name of the episode
+     */
+    private void updateName(String name){
+        reproducerViewModel.setName(name);
     }
 }
