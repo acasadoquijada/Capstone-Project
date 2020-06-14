c670c2287dce9751c68d47cd0d90788ba8b59a7f
acasadoquijada <acasadoquijada@gmail.com>
2020-06-14 22:49:45 +0200

    feat: add reproducer transition
    Now the slidingUpPanel containing the reproducer and information about
    the episode is hide by default. When the user clicks on an episode, the
    slidingPanel will appear.
    
    Relates: #13

--- Podcasfy/app/src/main/java/com/example/podcasfy/ui/MainFragment.java
+++ Podcasfy/app/src/main/java/com/example/podcasfy/ui/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.podcasfy.ui;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,17 +12,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.podcasfy.R;
 import com.example.podcasfy.databinding.MainFragmentBinding;
 import com.example.podcasfy.viewmodel.ReproducerViewModel;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
-public class MainFragment extends Fragment implements PodcastFragment.ItemClickListener {
+public class MainFragment extends Fragment {
 
     private MainFragmentBinding binding;
     private ReproducerViewModel reproducerViewModel;
+    private ExoPlayer player;
+    private int panelHeight;
 
     @Nullable
     @Override
@@ -31,14 +37,27 @@ public class MainFragment extends Fragment implements PodcastFragment.ItemClickL
 
         setupSlidingUpPanel();
 
+        setupMediaPlayer();
+
        return binding.getRoot();
+    }
+
+    private void setupPanelHeight(){
+        panelHeight = binding.slidingLayout.getPanelHeight();
     }
 
     /**
      * To setup the SlidingUpPanel, we add a Panel SlideListener that change the alpha value
-     * of the sliding elements to have a smooth transition when the panel is slided
+     * of the sliding elements to have a smooth transition when the panel is slided.
+     *
      */
     private void setupSlidingUpPanel(){
+
+        setupPanelHeight();
+
+        hideSlidingPanel();
+
+        disableSlidingPanelTouchResponsiveness();
 
         binding.slidingLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
             @Override
@@ -54,13 +73,49 @@ public class MainFragment extends Fragment implements PodcastFragment.ItemClickL
         });
     }
 
-    /**
-     * To calculate the new alpha value to guarantees a smooth transition when the panel is slided
-     * @param slideOffset needed for the calculation
-     * @return the new alpha value
-     */
-    private float calculateNewAlphaValue(float slideOffset){
-        return 1-(4*slideOffset);
+
+    private void hideSlidingPanel(){
+        changeVisibilitySlidingPanel(View.GONE);
+        changePanelHeight(0);
+        changeShadowHeight(0);
+    }
+
+
+    private void showSlidingPanel(){
+        changeVisibilitySlidingPanel(View.VISIBLE);
+        changePanelHeight(panelHeight);
+        changeShadowHeight(4);
+    }
+
+    private void changePanelHeight(int panelHeight){
+        binding.slidingLayout.setPanelHeight(panelHeight);
+    }
+
+    private void changeShadowHeight(int shadowHeight){
+        binding.slidingLayout.setShadowHeight(shadowHeight);
+    }
+
+    private void changeVisibilitySlidingPanel(int visibility){
+        changeVisibilitySlidingEpisodeImage(visibility);
+        changeVisibilitySlidingName(visibility);
+        changeVisibilitySlidingMediaReproducer(visibility);
+    }
+
+    private void changeVisibilitySlidingEpisodeImage(int visibility){
+        binding.reproducer.slingindEpisodeImage.setVisibility(visibility);
+    }
+
+
+    private void changeVisibilitySlidingName(int visibility){
+        binding.reproducer.slidingName.setVisibility(visibility);
+    }
+
+    private void changeVisibilitySlidingMediaReproducer(int visibility) {
+        binding.reproducer.slidingMediaReproducer.setVisibility(visibility);
+    }
+
+    private void disableSlidingPanelTouchResponsiveness(){
+        binding.slidingLayout.setTouchEnabled(false);
     }
 
     /**
@@ -71,11 +126,65 @@ public class MainFragment extends Fragment implements PodcastFragment.ItemClickL
 
         final float newAlphaValue = calculateNewAlphaValue(slideOffset);
 
+        changeAlphaValueSlingindEpisodeImage(newAlphaValue);
+        changeAlphaValueSlidingName(newAlphaValue);
+        changeAlphaValueSlidingMediaReproducer(newAlphaValue);
+    }
+
+    /**
+     * To calculate the new alpha value to guarantees a smooth transition when the panel is slided
+     * @param slideOffset needed for the calculation
+     * @return the new alpha value
+     */
+    private float calculateNewAlphaValue(float slideOffset){
+        return 1-(6*slideOffset);
+    }
+
+    /**
+     * To change the alpha value of the slingindEpisodeImage
+     */
+
+    private void changeAlphaValueSlingindEpisodeImage(float newAlphaValue){
         binding.reproducer.slingindEpisodeImage.setAlpha(newAlphaValue);
+    }
+
+    /**
+     * To change the alpha value of the slingindEpisodeImage
+     */
+
+    private void changeAlphaValueSlidingName(float newAlphaValue){
         binding.reproducer.slidingName.setAlpha(newAlphaValue);
+    }
+
+    /**
+     * To change the alpha value of the slingindEpisodeImage
+     */
+
+    private void changeAlphaValueSlidingMediaReproducer(float newAlphaValue){
         binding.reproducer.slidingMediaReproducer.setAlpha(newAlphaValue);
     }
 
+    /**
+     * To create the ExoPlayer, setPlayWhenReady and attach it to the corresponding PlayerView
+     */
+
+    private void setupMediaPlayer(){
+        createExoPlayer();
+        setPlayWhenReady();
+        setExoPlayerInPlayerView();
+    }
+
+    private void createExoPlayer(){
+        player =  ExoPlayerFactory.newSimpleInstance(requireContext());
+    }
+
+    private void setPlayWhenReady(){
+        player.setPlayWhenReady(true);
+    }
+
+    private void setExoPlayerInPlayerView(){
+        binding.reproducer.mainMediaReproducer.setPlayer(player);
+    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -114,9 +223,9 @@ public class MainFragment extends Fragment implements PodcastFragment.ItemClickL
      */
     private void updateSlidingUIName(String name){
         binding.reproducer.slidingName.setText(name);
-    }
 
-    @Override
-    public void onItemClick(int clickedItem) {
+        // These methods are here for testing purposes. Will be removed eventually
+        binding.slidingLayout.setTouchEnabled(true);
+        showSlidingPanel();
     }
 }
